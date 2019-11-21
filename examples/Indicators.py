4720f80472dc22dbe8c8a99af6aa54143bb29a8b
youer <673125641@qq.com>
2019-11-21 17:07:24 +0800

    detect dense status of ma

--- examples/Indicators.py
+++ examples/Indicators.py
@@ -21,3 +21,21 @@ def get_kdj(high, low, close, n, m1, m2):
     J = 3 * K - 2 * D
 
     return K, D, J
+
+
+
+def dense_detector(ser, ratio=0.01):
+    max_ = max(ser.values)
+    min_ = min(ser.values)
+    if ((max_ - min_) / max_) < 0.01:
+        return 1
+    return 0
+
+ma_lst = [5, 20, 60, 120]
+for ma in ma_lst:
+    stk_01["ma%s" % ma] = stk_01.rolling(ma).mean()
+
+ma_cols = ["ma%s" % i for i in ma_lst]
+stk_01.fillna(0, inplace=True)
+stk_01["dense"] = stk_01[ma_cols].apply(dense_detector, axis=1)
+stk_01[stk_01.dense > 0]
